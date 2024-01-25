e66830fbbdd138dfe9daa9b3819f8a9e66010f18
varshithno1 <luckyvarshith24@gmail.com>
2024-01-25 13:08:04 +0530

    Added Git.js
    it tells how the git hub commands work in git

---
+++ git.js
@@ -0,0 +1,26 @@
+// Commands in git
+
+// git init
+    // It a .git file which keeps track of all the changes in the folder.
+
+// git add {file name} / git add .
+    // It add the file to the list of files which it will push to a repository.
+    // Note: When we use "." it will add all the files to the list.
+
+// git commit -m First commit"
+    // It make a commit (The confirmed files which will be pushed) locally
+    // Note: "-m" Tells the title of the commit
+
+// git log
+    // Shows all the commits stored locally
+
+// git status
+    // Shows the status of all the files it is keeping track of
+
+// git remote add origin {repo https link}
+// git remote add {remote-name} {repo https link}
+    // Connects Github and git
+
+// git push -u origin master
+// git push -u {remote-name} {branch name}
+    // Pushes all the changes to the cloud i.e. the GitHub
